c1d178bb4f7ac0fe21e807941583e50a2c3ebe78
FernandoVelazco <daniel.fernando.velazco@gmail.com>
2026-02-23 20:55:50 -0500

    Se normaliza app module

--- backend-taskmaster/src/app.module.ts
+++ backend-taskmaster/src/app.module.ts
@@ -4,6 +4,8 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -20,6 +22,8 @@ import { AppService } from './app.service';
         uri: configService.get<string>('DATABASE_URI'),
       }),
     }),
+    AuthModule,
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
