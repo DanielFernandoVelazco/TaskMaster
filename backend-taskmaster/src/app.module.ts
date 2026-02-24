9bd451eab4f085acc1968fb70b6d1fe79fef6d35
FernandoVelazco <daniel.fernando.velazco@gmail.com>
2026-02-23 20:51:25 -0500

    Se normaliza module

--- backend-taskmaster/src/app.module.ts
+++ backend-taskmaster/src/app.module.ts
@@ -1,9 +1,26 @@
+// src/app.module.ts
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    // 1. Cargar variables de entorno
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true, // Hace que ConfigService esté disponible globalmente
+    }),
+    // 2. Configurar conexión a MongoDB
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('DATABASE_URI'),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
