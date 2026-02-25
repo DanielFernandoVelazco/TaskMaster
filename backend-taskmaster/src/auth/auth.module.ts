2593303348f1bd353aeb5870982a18789be7a41e
FernandoVelazco <daniel.fernando.velazco@gmail.com>
2026-02-25 16:33:12 -0500

    Se normaliza auth module

--- backend-taskmaster/src/auth/auth.module.ts
+++ backend-taskmaster/src/auth/auth.module.ts
@@ -1,9 +1,28 @@
+// src/auth/auth.module.ts
 import { Module } from '@nestjs/common';
-import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { UsersModule } from '../users/users.module';
+import { LocalStrategy } from './strategies/local.strategy';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET') || 'secretKey',
+        signOptions: { expiresIn: '7d' }, // El token expira en 7 días
+      }),
+    }),
+  ],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
   controllers: [AuthController],
-  providers: [AuthService]
 })
-export class AuthModule {}
+export class AuthModule { }
