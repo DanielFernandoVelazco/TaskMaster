3f2a4453caae3b050a5ce9fb29683e484dedfdfe
FernandoVelazco <daniel.fernando.velazco@gmail.com>
2026-02-26 17:58:53 -0500

    Se normaliza auth controller

--- backend-taskmaster/src/auth/auth.controller.ts
+++ backend-taskmaster/src/auth/auth.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+// src/auth/auth.controller.ts
+import { Controller, Post, Body, HttpCode, HttpStatus, UseGuards, Request } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { RegisterDto, LoginDto } from './dto/auth.dto';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private authService: AuthService) { }
+
+    @Post('register')
+    async register(@Body() registerDto: RegisterDto) {
+        return this.authService.register(registerDto);
+    }
+
+    @Post('login')
+    @HttpCode(HttpStatus.OK)
+    async login(@Body() loginDto: LoginDto) {
+        return this.authService.login(loginDto);
+    }
+
+    // Endpoint protegido de ejemplo para probar el JWT
+    @Post('profile')
+    @UseGuards(AuthGuard('jwt'))
+    getProfile(@Request() req) {
+        return req.user;
+    }
+}
