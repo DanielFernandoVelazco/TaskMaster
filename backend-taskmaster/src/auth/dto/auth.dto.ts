b1d271626a2a35ffeac0a66b5522954f9e6932e8
FernandoVelazco <daniel.fernando.velazco@gmail.com>
2026-02-26 17:59:15 -0500

    Se normaliza auth dto

--- backend-taskmaster/src/auth/dto/auth.dto.ts
+++ backend-taskmaster/src/auth/dto/auth.dto.ts
@@ -1,11 +1,28 @@
 // src/auth/dto/auth.dto.ts
+import { IsEmail, IsString, MinLength, MaxLength, Matches } from 'class-validator';
+
 export class RegisterDto {
+    @IsEmail({}, { message: 'Por favor proporciona un email válido' })
     email: string;
+
+    @IsString()
+    @MinLength(6, { message: 'La contraseña debe tener al menos 6 caracteres' })
+    @MaxLength(20, { message: 'La contraseña no puede tener más de 20 caracteres' })
+    @Matches(/(?:(?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+        message: 'La contraseña debe tener al menos una mayúscula, una minúscula y un número o carácter especial'
+    })
     password: string;
+
+    @IsString()
+    @MinLength(2, { message: 'El nombre debe tener al menos 2 caracteres' })
+    @MaxLength(50, { message: 'El nombre no puede tener más de 50 caracteres' })
     name: string;
 }
 
 export class LoginDto {
+    @IsEmail({}, { message: 'Por favor proporciona un email válido' })
     email: string;
+
+    @IsString()
     password: string;
 }
