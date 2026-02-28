30be103481bbf00477148760dba750318a6d16e8
FernandoVelazco <daniel.fernando.velazco@gmail.com>
2026-02-27 23:11:10 -0500

    Se normaliza columns module

--- backend-taskmaster/src/columns/columns.module.ts
+++ backend-taskmaster/src/columns/columns.module.ts
@@ -4,10 +4,12 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ColumnsService } from './columns.service';
 import { ColumnsController } from './columns.controller';
 import { Column, ColumnSchema } from './schemas/column.schema';
+import { BoardsModule } from '../boards/boards.module';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: Column.name, schema: ColumnSchema }])
+    MongooseModule.forFeature([{ name: Column.name, schema: ColumnSchema }]),
+    BoardsModule, // Importamos BoardsModule para usar BoardsService
   ],
   controllers: [ColumnsController],
   providers: [ColumnsService],
