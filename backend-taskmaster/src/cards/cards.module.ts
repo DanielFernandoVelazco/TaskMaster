566bb59810eae936b92c9bc3896437c3cdbb3955
FernandoVelazco <daniel.fernando.velazco@gmail.com>
2026-02-27 23:06:24 -0500

    Se normaliza cards module

--- backend-taskmaster/src/cards/cards.module.ts
+++ backend-taskmaster/src/cards/cards.module.ts
@@ -1,9 +1,16 @@
+// src/cards/cards.module.ts
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { CardsService } from './cards.service';
 import { CardsController } from './cards.controller';
+import { Card, CardSchema } from './schemas/card.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Card.name, schema: CardSchema }])
+  ],
+  controllers: [CardsController],
   providers: [CardsService],
-  controllers: [CardsController]
+  exports: [CardsService],
 })
-export class CardsModule {}
+export class CardsModule { }
